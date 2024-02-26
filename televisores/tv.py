7d2816fa4b3cee3d9276fd495eba0a8331fa5168
juan-manuel-henao <jhenaoro@unal.edu.co>
2024-02-25 19:49:03 -0500

    1

---
+++ televisores/tv.py
@@ -0,0 +1,64 @@
+class TV:
+    _numTV=0
+    def __init__(self,marca,estado):
+        self._marca=marca
+        self._estado=estado
+        self._canal=1
+        self._volumen=1
+        self._precio=500
+        self._control
+        TV._numTV+=1
+    
+    def setMarca(self,marca):
+        self._marca=marca
+    def getMarca(self):
+        return self._marca
+    
+    def setCanal(self,canal):
+        if (self._estado==True and canal>=1 and canal<=120):
+            self._canal=canal
+        else:
+            pass
+    def getCanal(self):
+        return self._canal
+    
+    def setVolumen(self,volumen):
+        if (self._estado==True and volumen>=0 and volumen<=7):
+            self._volumen=volumen
+        else:
+            pass
+    def getVolumen(self):
+        return self._volumen
+    
+    def setPrecio(self,precio):
+        self._precio=precio
+    def getPrecio(self):
+        return self._precio
+    
+    def setControl(self,control):
+        self._control=control
+    def getControl(self):
+        return self._control
+    @classmethod
+    def setNumTV(cls,numTV):
+        cls._numTV=numTV
+    @classmethod
+    def getNumTV(cls):
+        return cls._numTV
+    
+    def turnOn(self):
+        self._estado=True
+    def turnOff(self):
+        self._estado=False
+    def getEstado(self):
+        return self._estado
+    
+    def canalUp(self):
+        self.setCanal(self._canal+1)
+    def canalDown(self):
+        self.setCanal(self._canal-1)
+    
+    def volumenUp(self):
+        self.setVolumen(self._volumen+1)
+    def volumenDown(self):
+        self.setVolumen(self._volumen-1)
